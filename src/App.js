132bc14911840873089e6d3d60b26007264edd35
wrendev <wrenfajardo123@gmail.com>
2021-03-06 21:36:28 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>
+        <nav
+          class="navbar navbar-expand-lg  fixed-top navbar-light"
+          id="mainNav"
+          style={{ backgroundColor: "#8A31FF" }}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ display: "flex" }}>
+            <div>
+              <img style={{ maxWidth: "100px", maxHeight: "100px" }} alt="" />
+            </div>
+
+            <div
+              class="collapse navbar-collapse"
+              id="navbarResponsive"
+              style={{ width: "100%", "flex-grow": "0" }}
+            >
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    href="/Settings/2"
+                    title="Settings/2"
+                    style={{
+                      color: "white",
+                      fontWeight: 700,
+                    }}
+                  >
+                    SEARCH
+                  </a>
+                </li>
+
+                <li style={{ cursor: "pointer" }} class="nav-item">
+                  <a
+                    style={{
+                      cursor: "pointer",
+                      color: "white",
+                      fontWeight: 700,
+                    }}
+                    class="nav-link"
+                  >
+                    PINNED
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <button
+              class="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
